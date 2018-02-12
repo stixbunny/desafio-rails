5b352c1ea408ea19842fa30a0e66799568219e5e
Milenko Castillo <milenko.castillo@gmail.com>
2018-02-11 23:29:42 -0300

    Tercer commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'pages/two'
 
+  get 'pages/three'
+
   root 'pages#one'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
